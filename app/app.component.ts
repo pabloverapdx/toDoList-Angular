f7f1056dc9e4f4949a59ae5a61bd35fd283b5bcf
Niem Nguyen and Pablo Vera <pablo@>
2016-05-31 16:29:33 -0700

    add all progress of today Tuesday 5/31

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,76 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+    selector: 'task-display',
+    inputs: ['task'],
+  template: `
+    <h3>{{ task.description }}</h3>
+  `
+})
+
+export class TaskComponent {
+  public task: Task;
+}
+
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  directives: [TaskComponent],
+  template: `
+  <h3 *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask">
+    {{ currentTask.description }}
+  </h3>
+  `
+})
+
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
 
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
-    <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+    <div class="container" >
+      <h1>To-Do List</h1>
+      <task-list [taskList]="tasks" (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
     </div>
   `
 })
 
 export class AppComponent {
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+      new Task("Create To-do app.", 0),
+      new Task("Learn Angular", 1),
+      new Task("Learn Ember", 2),
+      new Task("Master Javascript", 3)
+    ];
+
+  }
+  taskWasSelected(clickedTask: Task): void {
+      console.log(clickedTask, 'parent');
+  }
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
 }
